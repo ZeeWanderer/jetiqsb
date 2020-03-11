bc3ba1d41d5fe89a89c95aba8eb3eb352074956e
Maxim Kulyk <mxx888777@gmail.com>
2020-03-11 13:35:02 +0200

    [version] 0.1.1

--- jetiqsb.user.js
+++ jetiqsb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jetiq_soset_bibu
 // @namespace    http://tampermonkey.net/
-// @version      0.0.0
+// @version      0.1.1
 // @description  try to take over the world!
 // @author       You
 // @match        *://test.vntu.edu.ua/*
