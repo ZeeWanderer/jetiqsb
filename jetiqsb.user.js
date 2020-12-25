8ec762662f108069a725c91eeb671933aa075bc5
ZeeWanderer <mxx888777@gmail.com>
2020-12-25 20:50:12 +0200

    bump version

--- jetiqsb.user.js
+++ jetiqsb.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         jetiq_soset_bibu
 // @namespace    http://tampermonkey.net/
-// @version      0.2.9
+// @version      0.2.10
 // @description  try to take over the world!
 // @author       You
 // @match        *://test.vntu.edu.ua/*
